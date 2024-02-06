d6b7d1f8d996ab1d3aa1dc8e0261a62dbeb6263b
Jolo Tan <jolotan416@gmail.com>
2024-02-06 11:15:53 +0800

    Fix Pacman movement

--- Assets/Scripts/Pacman/PacmanMovementController.cs
+++ Assets/Scripts/Pacman/PacmanMovementController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,23 +8,76 @@ namespace Pacman
 {
     public class PacmanMovementController : MonoBehaviour
     {
+        private static readonly float VERTICAL_ROTATION = 180f;
+        private static readonly float HORIZONTAL_ROTATION = 90f;
+
         [SerializeField]
         private float movementSpeed = 5f;
 
         private Rigidbody rb;
+        private float horizontalInput = 0f;
+        private float verticalInput = 0f;
 
         private void Start()
         {
             rb = GetComponent<Rigidbody>();
         }
 
-        // Update is called once per frame
-        void Update()
+        private void Update()
         {
-            transform.position += new Vector3(
-                Input.GetAxis(Constants.HORIZONTAL_AXIS) * movementSpeed,
-                0f,
-                Input.GetAxis(Constants.VERTICAL_AXIS) * movementSpeed);
+            float newHorizontalInput = Input.GetAxis(Constants.HORIZONTAL_AXIS);
+            float newVerticalInput = Input.GetAxis(Constants.VERTICAL_AXIS);
+
+            if (newHorizontalInput != 0f)
+            {
+                horizontalInput = Mathf.Sign(newHorizontalInput);
+                verticalInput = 0f;
+            } else if (newVerticalInput != 0f)
+            {
+                horizontalInput = 0f;
+                verticalInput = Mathf.Sign(newVerticalInput);
+            }
+
+            RotateFromInput();
+        }
+
+        private void FixedUpdate()
+        {
+            MovePacman();
+        }
+
+        void RotateFromInput()
+        {
+            if (horizontalInput != 0)
+            {
+                transform.rotation = Quaternion.AngleAxis(
+                    (horizontalInput > 0f) ? -HORIZONTAL_ROTATION : HORIZONTAL_ROTATION,
+                    Vector3.up);
+
+                return;
+            }
+
+            if (verticalInput != 0)
+            {
+                transform.rotation = Quaternion.AngleAxis(
+                    (verticalInput > 0f) ? VERTICAL_ROTATION : 0f,
+                    Vector3.up);
+            }
+        }
+
+        void MovePacman()
+        {
+            if (horizontalInput != 0)
+            {
+                rb.AddForce(Vector3.right * movementSpeed * horizontalInput, ForceMode.VelocityChange);
+
+                return;
+            }
+
+            if (verticalInput != 0)
+            {
+                rb.AddForce(Vector3.forward * movementSpeed * verticalInput, ForceMode.VelocityChange);
+            }
         }
     }
 }
